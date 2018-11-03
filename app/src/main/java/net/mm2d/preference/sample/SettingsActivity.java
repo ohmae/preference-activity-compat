c82aa8b1d389e7324ef2d3e66888a7cd52a671f4
OHMAE Ryosuke <ryo@mm2d.net>
2018-11-03 12:06:00 +0900

    :sparkles: CustomPreferenceFragmentCompat

--- app/src/main/java/net/mm2d/preference/sample/SettingsActivity.java
+++ app/src/main/java/net/mm2d/preference/sample/SettingsActivity.java
@@ -1,11 +1,15 @@
 package net.mm2d.preference.sample;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import net.mm2d.preference.Header;
 import net.mm2d.preference.PreferenceActivityCompat;
@@ -18,7 +22,12 @@ import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceGroupAdapter;
 import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceViewHolder;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
 
 public class SettingsActivity extends PreferenceActivityCompat {
     private static final OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = (preference, value) -> {
@@ -89,7 +98,7 @@ public class SettingsActivity extends PreferenceActivityCompat {
         return super.onOptionsItemSelected(item);
     }
 
-    public static class GeneralPreferenceFragment extends PreferenceFragmentCompat {
+    public static class GeneralPreferenceFragment extends CustomPreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(
                 final Bundle savedInstanceState,
@@ -104,7 +113,7 @@ public class SettingsActivity extends PreferenceActivityCompat {
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class NotificationPreferenceFragment extends PreferenceFragmentCompat {
+    public static class NotificationPreferenceFragment extends CustomPreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(
                 final Bundle savedInstanceState,
@@ -116,7 +125,7 @@ public class SettingsActivity extends PreferenceActivityCompat {
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class DataSyncPreferenceFragment extends PreferenceFragmentCompat {
+    public static class DataSyncPreferenceFragment extends CustomPreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(
                 final Bundle savedInstanceState,
@@ -124,6 +133,38 @@ public class SettingsActivity extends PreferenceActivityCompat {
             addPreferencesFromResource(R.xml.pref_data_sync);
             setHasOptionsMenu(true);
             bindPreferenceSummaryToValue(findPreference("sync_frequency"));
+        }
+    }
+
+    private static abstract class CustomPreferenceFragmentCompat extends PreferenceFragmentCompat {
+        @Override
+        protected Adapter onCreateAdapter(final PreferenceScreen preferenceScreen) {
+            return new CustomPreferenceGroupAdapter(preferenceScreen);
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    private static class CustomPreferenceGroupAdapter extends PreferenceGroupAdapter {
+        CustomPreferenceGroupAdapter(final PreferenceGroup preferenceGroup) {
+            super(preferenceGroup);
+        }
+
+        @NonNull
+        @Override
+        public PreferenceViewHolder onCreateViewHolder(
+                final ViewGroup parent,
+                final int viewType) {
+            final PreferenceViewHolder holder = super.onCreateViewHolder(parent, viewType);
+            final View icon = holder.findViewById(android.R.id.icon);
+            if (icon != null) {
+                final ViewParent p = icon.getParent();
+                if (p instanceof View) {
+                    final View parentView = (View) p;
+                    parentView.setMinimumWidth(0);
+                    parentView.setPadding(0,0,0,0);
+                }
+            }
+            return holder;
         }
     }
 }
