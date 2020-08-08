ef2737c767986d7029234afb0c4bb13f8e7458be
OHMAE Ryosuke <ryo@mm2d.net>
2020-08-09 06:37:50 +0900

    💚buildSrc

---
+++ buildSrc/src/main/java/build/Properties.kt
@@ -0,0 +1,17 @@
+package build
+
+object Properties {
+    const val groupId: String = "net.mm2d"
+
+    private const val versionMajor: Int = 0
+    private const val versionMinor: Int = 2
+    private const val versionPatch: Int = 3
+    const val versionName: String = "$versionMajor.$versionMinor.$versionPatch"
+    const val versionCode: Int = versionMajor * 10000 + versionMinor * 100 + versionPatch
+
+    object Url {
+        const val site: String = "https://github.com/ohmae/preference-activity-compat"
+        const val github: String = "https://github.com/ohmae/preference-activity-compat"
+        const val scm: String = "scm:git:https://github.com/ohmae/preference-activity-compat.git"
+    }
+}
