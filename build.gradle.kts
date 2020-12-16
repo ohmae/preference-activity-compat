9d6ceba6e04ae6795799b0b4c728e61fe928f8d3
OHMAE Ryosuke <ryo@mm2d.net>
2020-12-16 22:21:21 +0900

    🆙kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0")
-        classpath(kotlin("gradle-plugin", version = "1.4.10"))
+        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath(kotlin("gradle-plugin", version = "1.4.21"))
 
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.29.0")
