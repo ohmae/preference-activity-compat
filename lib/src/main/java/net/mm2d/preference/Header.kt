73651ef287381ccb9bef4c182ef1b284567722d9
OHMAE Ryosuke <ryo@mm2d.net>
2021-02-07 12:23:16 +0900

    📖add dokka comment

--- lib/src/main/java/net/mm2d/preference/Header.kt
+++ lib/src/main/java/net/mm2d/preference/Header.kt
@@ -17,21 +17,60 @@ import android.text.TextUtils
 import androidx.annotation.StringRes
 import net.mm2d.preference.PreferenceActivityCompatDelegate.Companion.HEADER_ID_UNDEFINED
 
+/**
+ * Description of a single Header item that the user can select.
+ */
 class Header : Parcelable {
+    /**
+     * Identifier for this header, to correlate with a new list when it is updated.
+     */
     var id = HEADER_ID_UNDEFINED
+    /**
+     * Resource ID of title of the header that is shown to the user.
+     */
     @StringRes
     var titleRes: Int = 0
+    /**
+     * Title of the header that is shown to the user.
+     */
     var title: CharSequence? = null
+    /**
+     * Resource ID of optional summary describing what this header controls.
+     */
     @StringRes
     var summaryRes: Int = 0
+    /**
+     * Optional summary describing what this header controls.
+     */
     var summary: CharSequence? = null
+    /**
+     * Resource ID of optional text to show as the title in the bread crumb.
+     */
     @StringRes
     var breadCrumbTitleRes: Int = 0
+    /**
+     * Optional text to show as the title in the bread crumb.
+     */
     var breadCrumbTitle: CharSequence? = null
+    /**
+     * Optional icon resource to show for this header.
+     */
     var iconRes: Int = 0
+    /**
+     * Full class name of the fragment to display when this header is selected.
+     */
     var fragment: String? = null
+    /**
+     * Optional arguments to supply to the fragment when it is instantiated.
+     */
     var fragmentArguments: Bundle? = null
+    /**
+     * Intent to launch when the preference is selected.
+     */
     var intent: Intent? = null
+    /**
+     * Optional additional data for use by subclasses of PreferenceActivityCompat.
+     */
     var extras: Bundle? = null
 
     constructor()
@@ -40,12 +79,21 @@ class Header : Parcelable {
         readFromParcel(parcel)
     }
 
+    /**
+     * Return the currently set title.
+     */
     fun getTitle(res: Resources): CharSequence? =
         getText(res, titleRes, title)
 
+    /**
+     * Return the currently set summary.
+     */
     fun getSummary(res: Resources): CharSequence? =
         getText(res, summaryRes, summary)
 
+    /**
+     * Return the currently set bread crumb title.
+     */
     fun getBreadCrumbTitle(res: Resources): CharSequence? =
         getText(res, breadCrumbTitleRes, breadCrumbTitle)
 
--- lib/src/main/java/net/mm2d/preference/PreferenceActivityCompat.kt
+++ lib/src/main/java/net/mm2d/preference/PreferenceActivityCompat.kt
@@ -19,6 +19,13 @@ import androidx.fragment.app.Fragment
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
+/**
+ * This is a compatibility library of PreferenceActivity.
+ *
+ * This class can be used in much the same way as PreferenceActivity.
+ * Moreover, the material design is applied,
+ * It is also possible to manage PreferenceFragmentCompat instead of PreferenceFragment.
+ */
 @SuppressLint("Registered")
 open class PreferenceActivityCompat : AppCompatActivity(),
     PreferenceActivityCompatDelegate.Connector,
@@ -26,12 +33,18 @@ open class PreferenceActivityCompat : AppCompatActivity(),
 
     private lateinit var delegate: PreferenceActivityCompatDelegate
 
+    /**
+     * Get the cursor row ID of the currently selected list item.
+     */
     open val selectedItemPosition: Int
         get() = delegate.selectedItemPosition
 
     open val headers: List<Header>
         get() = delegate.headers
 
+    /**
+     * Returns true if this activity is showing multiple panes -- the headers and a preference fragment.
+     */
     open val isMultiPane: Boolean
         get() = delegate.isMultiPane
 
@@ -64,12 +77,36 @@ open class PreferenceActivityCompat : AppCompatActivity(),
         super.onBackPressed()
     }
 
+    /**
+     * Called to determine if the activity should run in multi-pane mode.
+     * The default implementation returns true if the screen is large enough.
+     */
     override fun onIsMultiPane(): Boolean = resources.getBoolean(R.bool.mm2d_pac_dual_pane)
 
+    /**
+     * Called to determine whether the header list should be hidden.
+     * The default implementation returns the value given in [EXTRA_NO_HEADERS] or false if it is not supplied.
+     * This is set to false, for example, when the activity is being re-launched to show a particular preference activity.
+     */
     override fun onIsHidingHeaders(): Boolean = delegate.onIsHidingHeaders()
 
+    /**
+     * Called when the activity needs its list of headers build.
+     * By implementing this and adding at least one item to the list,
+     * you will cause the activity to run in its modern fragment mode.
+     * Note that this function may not always be called; for example,
+     * if the activity has been asked to display a particular fragment without the header list,
+     * there is no need to build the headers.
+     *
+     * Typical implementations will use [loadHeadersFromResource] to fill in the list from a resource.
+     */
     override fun onBuildHeaders(target: MutableList<Header>) = Unit
 
+    /**
+     * Subclasses should override this method and verify that the given fragment is a valid type to be attached to this activity.
+     * The default implementation returns true for apps built for android:targetSdkVersion older than Build.VERSION_CODES.KITKAT.
+     * For later versions, it will throw an exception.
+     */
     override fun isValidFragment(fragmentName: String?): Boolean =
         if (applicationInfo.targetSdkVersion >= VERSION_CODES.KITKAT) {
             throw RuntimeException(
@@ -82,28 +119,51 @@ open class PreferenceActivityCompat : AppCompatActivity(),
             true
         }
 
+    /**
+     * Returns true if this activity is currently showing the header list.
+     */
     open fun hasHeaders(): Boolean = delegate.hasHeaders()
 
+    /**
+     * Call when you need to change the headers being displayed.
+     * Will result in onBuildHeaders() later being called to retrieve the new list.
+     */
     open fun invalidateHeaders() {
         delegate.invalidateHeaders()
     }
 
+    /**
+     * Parse the given XML file as a header description, adding each parsed Header into the target list.
+     */
     open fun loadHeadersFromResource(@XmlRes resId: Int, target: MutableList<Header>) {
         delegate.loadHeadersFromResource(resId, target)
     }
 
+    /**
+     * Set a footer that should be shown at the bottom of the header list.
+     */
     open fun setListFooter(view: View) {
         delegate.setListFooter(view)
     }
 
+    /**
+     * When in two-pane mode, switch the fragment pane to show the given preference fragment.
+     */
     open fun switchToHeader(fragmentName: String, args: Bundle?) {
         delegate.switchToHeader(fragmentName, args)
     }
 
+    /**
+     * When in two-pane mode, switch to the fragment pane to show the given preference fragment.
+     */
     open fun switchToHeader(header: Header) {
         delegate.switchToHeader(header)
     }
 
+    /**
+     * Called when the user has clicked on a Preference that has a fragment class name associated with it.
+     * The implementation to should instantiate and switch to an instance of the given fragment.
+     */
     override fun onPreferenceStartFragment(
         caller: PreferenceFragmentCompat,
         pref: Preference
@@ -112,6 +172,12 @@ open class PreferenceActivityCompat : AppCompatActivity(),
         return true
     }
 
+    /**
+     * Called by [startWithFragment] when in single-pane mode,
+     * to build an Intent to launch a new activity showing the selected fragment.
+     * The default implementation constructs an Intent that re-launches the current activity with
+     * the appropriate arguments to display the fragment.
+     */
     open fun onBuildStartFragmentIntent(
         fragmentName: String,
         arguments: Bundle?,
@@ -126,6 +192,11 @@ open class PreferenceActivityCompat : AppCompatActivity(),
         it.putExtra(EXTRA_NO_HEADERS, true)
     }
 
+    /**
+     * Start a new instance of this activity, showing only the given preference fragment.
+     * When launched in this mode, the header list will be hidden and
+     * the given preference fragment will be instantiated and fill the entire activity.
+     */
     @JvmOverloads
     open fun startWithFragment(
         fragmentName: String,
