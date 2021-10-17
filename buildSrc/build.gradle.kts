50a22a028fd573c4f622304027b82a06d54aa67b
OHMAE Ryosuke <ryo@mm2d.net>
2021-10-17 23:55:31 +0900

    🆙AGP

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.5.31"
     `kotlin-dsl`
 }
 
@@ -10,6 +10,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.android.tools.build:gradle:7.0.1")
+    implementation("com.android.tools.build:gradle:7.0.3")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
 }
