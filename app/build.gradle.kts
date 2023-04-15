92373c25e0d0178582eec186d1e38f01fe24ab37
OHMAE Ryosuke <ryo@mm2d.net>
2023-04-15 09:09:41 +0900

    🐛targetJvm

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,9 +18,15 @@ android {
         versionName = "1.0"
         vectorDrawables.useSupportLibrary = true
     }
+    kotlin {
+        jvmToolchain(11)
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
     }
     buildTypes {
         getByName("release") {
